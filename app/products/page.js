746b027168ba21b012119de8f4bd8b5f5eecf36d
devnader1 <devnader7@gmail.com>
2025-07-11 05:47:28 +0300

    Add files via upload

---
+++ app/products/page.js
@@ -0,0 +1,16 @@
+import Blog from "@/components/Blog";
+import Category from "@/components/Category";
+import Feature from "@/components/Feature";
+import Gallery from "@/components/Gallery";
+
+export default function Product() {
+  return (
+    <>
+      <h1>ProductPage</h1>
+      <Blog />
+      <Gallery />
+      <Category />
+      <Feature />
+    </>
+  );
+}
